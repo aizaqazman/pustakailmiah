51dce2b9ca9e9f5c7c343993f6f597e06a77d450
alysssa7 <96166276+alysssa7@users.noreply.github.com>
2021-12-27 15:12:54 +0800

    Update about.php

--- src/about.php
+++ src/about.php
@@ -1,4 +1,3 @@
-<?php include('navbar.php'); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,7 +6,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
-  <link  href="contact.css" rel="stylesheet">
   <link href="about.css" rel="stylesheet" type="text/css">
   <title>About Us</title>
   
@@ -134,120 +132,116 @@
 	
 	<!-- Footer -->
   <footer class="text-center text-lg-start bg-grey-77 text-light">
-    <!-- Section: Links  -->
-    <section class="p-2">
-      <div class="container text-center text-md-start mt-5">
-        <!-- Grid row -->
-        <div class="row mt-3">
-          <!-- Grid column -->
-          <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-            <!-- Content -->
-            <h6 class="text-uppercase fw-bold mb-4">
-              About Us
-            </h6>
-            <hr>
-            <p>
-            Kedai Buku, Al-Quran & Alat Tulis dan Servis Perkhidmatan antaranya :<br>
-            (1) Buku Terkini & Bestseller<br>
-            (2) Bahan Perpustakaan<br>
-            (3) Alat Bantu Mengajar<br>
-            (4) Alat Tulis<br>
-            (5) Makanan Sunnah<br>
-            </p>
-            <div>
-              <a href="" class="socmed me-4 text-reset text-decoration-none">
-                <i class="fab fa-whatsapp"></i>
-              </a>
-              <a href="" class="socmed me-4 text-reset text-decoration-none">
-                <i class="fab fa-facebook-f"></i>
-              </a>
-              <a href="" class="socmed me-4 text-reset text-decoration-none">
-                <i class="fab fa-telegram-plane"></i>
-              </a>
-              <a href="" class="socmed me-4 text-reset text-decoration-none">
-                <i class="fab fa-twitter"></i>
-              </a>
-              <a href="" class="socmed me-4 text-reset text-decoration-none">
-                <i class="far fa-envelope"></i>
-              </a>
-              <a href="" class="socmed me-4 text-reset text-decoration-none">
-                <i class="fab fa-google"></i>
-              </a>
-              <a href="" class="socmed me-4 text-reset text-decoration-none">
-                <i class="fab fa-instagram"></i>
-              </a>
-              <a href="" class="socmed me-4 text-reset text-decoration-none">
-                <i class="fab fa-pinterest"></i>
-              </a>
+        <!-- Section: Links  -->
+        <section class="p-2">
+        <div class="container text-center text-md-start mt-5">
+            <!-- Grid row -->
+            <div class="row mt-3">
+            <!-- Grid column -->
+            <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                <!-- Content -->
+                <h6 class="text-uppercase fw-bold mb-4">
+                About Us
+                </h6>
+                <hr>
+                <p>
+                Kedai Buku, Al-Quran & Alat Tulis dan Servis Perkhidmatan antaranya :<br>
+                (1) Buku Terkini & Bestseller<br>
+                (2) Bahan Perpustakaan<br>
+                (3) Alat Bantu Mengajar<br>
+                (4) Alat Tulis<br>
+                (5) Makanan Sunnah<br>
+                </p>
+                <div>
+                <a href="" class="socmed me-4 text-reset text-decoration-none">
+                    <i class="fab fa-whatsapp"></i>
+                </a>
+                <a href="" class="socmed me-4 text-reset text-decoration-none">
+                    <i class="fab fa-facebook-f"></i>
+                </a>
+                <a href="" class="socmed me-4 text-reset text-decoration-none">
+                    <i class="fab fa-telegram-plane"></i>
+                </a>
+                <a href="" class="socmed me-4 text-reset text-decoration-none">
+                    <i class="fab fa-twitter"></i>
+                </a>
+                <a href="" class="socmed me-4 text-reset text-decoration-none">
+                    <i class="far fa-envelope"></i>
+                </a>
+                <a href="" class="socmed me-4 text-reset text-decoration-none">
+                    <i class="fab fa-google"></i>
+                </a>
+                <a href="" class="socmed me-4 text-reset text-decoration-none">
+                    <i class="fab fa-instagram"></i>
+                </a>
+                <a href="" class="socmed me-4 text-reset text-decoration-none">
+                    <i class="fab fa-pinterest"></i>
+                </a>
+                </div>
             </div>
-          </div>
-          <!-- Grid column -->
-
-          <!-- Grid column -->
-          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-            <!-- Links -->
-            <h6 class="text-uppercase fw-bold mb-4">
-              Latest News
-            </h6>
-            <hr>
-            <p>
-              <a href="#" class="news-link text-light text-decoration-none">
-                <span class="font-monospace">6th May - </span>
-                Website Launched!
-              </a>
-            </p>
-          </div>
-          <!-- Grid column -->
-
-          <!-- Grid column -->
-          <div class="col-md-4 col-lg-2 col-xl-2 mx-auto mb-4">
-            <!-- Links -->
-            <h6 class="text-uppercase fw-bold mb-4">
-              Sign Up For Newsletter
-            </h6>
-            <hr>
-            <form>
-              <input type="text" class="rounded-pill mb-2 p-1 form-control" name="email">
-              <button class="btn bg-maroon rounded-pill p-2 text-light">Sign Up</button>
-            </form>
-          </div>
-          <!-- Grid column -->
-
-          <!-- Grid column -->
-          <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-            <!-- Links -->
-            <h6 class="text-uppercase fw-bold mb-4">
-              Contact
-            </h6>
-            <p><i class="fas fa-home me-3"></i> Pustaka Ilmiah, 1150-a, Jalan Pasir Panjang, 21100 Kuala Terengganu, Terengganu</p>
-            <p>
-              <i class="fas fa-envelope me-3"></i>
-              pustaka_ilmiah@yahoo.com
-            </p>
-            <p><i class="fas fa-phone me-3"></i> 09-6226996</p>
-            <p><i class="fas fa-print me-3"></i> 09-6226996</p>
-          </div>
-          <!-- Grid column -->
+            <!-- Grid column -->
+            <!-- Grid column -->
+            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                <!-- Links -->
+                <h6 class="text-uppercase fw-bold mb-4">
+                Latest News
+                </h6>
+                <hr>
+                <p>
+                <a href="#" class="news-link text-light text-decoration-none">
+                    <span class="font-monospace">6th May - </span>
+                    Website Launched!
+                </a>
+                </p>
+            </div>
+            <!-- Grid column -->
+            <!-- Grid column -->
+            <div class="col-md-4 col-lg-2 col-xl-2 mx-auto mb-4">
+                <!-- Links -->
+                <h6 class="text-uppercase fw-bold mb-4">
+                Sign Up For Newsletter
+                </h6>
+                <hr>
+                <form>
+                <input type="text" class="rounded-pill mb-2 p-1 form-control" name="email">
+                <button class="btn bg-maroon rounded-pill p-2 text-light">Sign Up</button>
+                </form>
+            </div>
+            <!-- Grid column -->
+            <!-- Grid column -->
+            <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                <!-- Links -->
+                <h6 class="text-uppercase fw-bold mb-4">
+                Contact
+                </h6>
+                <p><i class="fas fa-home me-3"></i> Pustaka Ilmiah, 1150-a, Jalan Pasir Panjang, 21100 Kuala Terengganu, Terengganu</p>
+                <p>
+                <i class="fas fa-envelope me-3"></i>
+                pustaka_ilmiah@yahoo.com
+                </p>
+                <p><i class="fas fa-phone me-3"></i> 09-6226996</p>
+                <p><i class="fas fa-print me-3"></i> 09-6226996</p>
+            </div>
+            <!-- Grid column -->
+            </div>
+            <!-- Grid row -->
         </div>
-        <!-- Grid row -->
-      </div>
-    </section>
-    <!-- Section: Links  -->
-
-    <!-- Copyright -->
-    <div class="text-center p-4 bg-grey-5b d-md-flex justify-content-between">
-      <div>
-        Copyright Pustaka Ilmiah ©️ 2021 |
-        <a class="text-reset fw-bold" href="#">website.com</a>
-      </div>
-      <div>
-        <i class="payment-icon mx-1 fab fa-cc-visa"></i>
-        <i class="payment-icon mx-1 fab fa-cc-mastercard"></i>
-        <i class="payment-icon mx-1 fas fa-money-check"></i>
-      </div>
-    </div>
-    <!-- Copyright -->
-  </footer>
+        </section>
+        <!-- Section: Links  -->
+        <!-- Copyright -->
+        <div class="text-center p-4 bg-grey-5b d-md-flex justify-content-between">
+        <div>
+            Copyright Pustaka Ilmiah ©️ 2021 |
+            <a class="text-reset fw-bold" href="#">website.com</a>
+        </div>
+        <div>
+            <i class="payment-icon mx-1 fab fa-cc-visa"></i>
+            <i class="payment-icon mx-1 fab fa-cc-mastercard"></i>
+            <i class="payment-icon mx-1 fas fa-money-check"></i>
+        </div>
+        </div>
+        <!-- Copyright -->
+    </footer>
 
 </body>
 </html>
