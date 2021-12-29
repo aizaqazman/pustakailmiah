75cbdd0f06575d84bfae59ff8052c17e18d218c6
Irfanadl <Irfan.adl410622@gmail.com>
2021-12-30 00:47:14 +0800

    Update order-response.php

--- src/order-response.php
+++ src/order-response.php
@@ -1,10 +1,12 @@
 <?php
+echo "<h1>Order Response Page</h1>";
 if(isset($_COOKIE['orders_list'])) {
+  echo "Order List(from local cookie):<br>";
   $cookie_data = stripslashes($_COOKIE['orders_list']);
   $order_data = json_decode($cookie_data, true);
   if(!empty($order_data)) {
     foreach($order_data as $keys => $values){
-      echo $values['order_id']."<br>";
+      echo "Order ID: ".$values['order_id']."<br>";
     }
   } else {
     echo "no order";
