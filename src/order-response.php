35225ff0d64dc96d1d2c66f73351fbbe4cd43cfe
Irfanadl <Irfan.adl410622@gmail.com>
2021-12-30 02:43:15 +0800

    Update order-response.php

--- src/order-response.php
+++ src/order-response.php
@@ -1,18 +1,24 @@
 <?php
-echo "<h1>Order Response Page</h1>";
-if(isset($_COOKIE['orders_list'])) {
-  echo "Order List(from local cookie):<br>";
-  $cookie_data = stripslashes($_COOKIE['orders_list']);
-  $order_data = json_decode($cookie_data, true);
-  if(!empty($order_data)) {
-    foreach($order_data as $keys => $values){
-      echo "Order ID: ".$values['order_id']."<br>";
-    }
-  } else {
-    echo "no order";
-  }
-}
-else {
-  echo "no cookie order";
-}
+// echo "<h1>Order Response Page</h1>";
+// if(isset($_COOKIE['orders_list'])) {
+//   echo "Order List(from local cookie):<br>";
+//   $cookie_data = stripslashes($_COOKIE['orders_list']);
+//   $order_data = json_decode($cookie_data, true);
+//   if(!empty($order_data)) {
+//     foreach($order_data as $keys => $values){
+//       echo "Order ID: ".$values['order_id']."<br>";
+//     }
+//   } else {
+//     echo "no order";
+//   }
+// }
+// else {
+//   echo "no cookie order";
+// }
+echo '<pre>';
+echo 'GET Data';
+print_r($_GET);
+echo 'POST Data';
+print_r($_POST);
+echo '</pre>';
 ?>
