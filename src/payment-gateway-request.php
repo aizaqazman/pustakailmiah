28c928d10df39b464298fdaf90f3a2ff81e76df5
Irfanadl <Irfan.adl410622@gmail.com>
2021-12-30 02:46:55 +0800

    Commented payment gateway API (jangan sesuka hati nak run)

--- src/payment-gateway-request.php
+++ src/payment-gateway-request.php
@@ -10,49 +10,51 @@ $stmt -> execute();
 $result = $stmt -> get_result();
 $stmt -> close();
 $row = $result -> fetch_assoc();
+echo "Order ID: ".$order_id;
+echo "<br><br>Jangan uncomment api request data, tu betul2 payment gateway, kalau nak test sekali 2 kali takpe, jangan byk kali"
 
-// api request data
-$post_data = array(
-    'userSecretKey'           => $secret_key,
-    'categoryCode'            => $category_code,
-    'billName'                => 'PustakaIlmiah Order Payment',
-    'billDescription'         => 'Order ID: '.$order_id,
-    'billPriceSetting'        => 1,
-    'billPayorInfo'           => 1,
-    'billAmount'              => $row['order_final_amount'] * 100,
-    'billReturnUrl'           => 'http://bizapp.my',
-    'billCallbackUrl'         => 'http://bizapp.my/paystatus',
-    'billExternalReferenceNo' => $order_id,
-    'billTo'                  => $row['first_name']." ".$row['last_name'],
-    'billEmail'               => $row['email'],
-    'billPhone'               => $row['phone_number'],
-    'billSplitPayment'        => 0,
-    'billSplitPaymentArgs'    => '',
-    'billPaymentChannel'      => '0',
-    'billContentEmail'        => 'Thank you for purchasing our product!',
-    'billChargeToCustomer'    => 1
-);
+// // api request data
+// $post_data = array(
+//     'userSecretKey'           => $secret_key,
+//     'categoryCode'            => $category_code,
+//     'billName'                => 'PustakaIlmiah Order Payment',
+//     'billDescription'         => 'Order ID: '.$order_id,
+//     'billPriceSetting'        => 1,
+//     'billPayorInfo'           => 1,
+//     'billAmount'              => $row['order_final_amount'] * 100,
+//     'billReturnUrl'           => 'http://bizapp.my',
+//     'billCallbackUrl'         => 'http://bizapp.my/paystatus',
+//     'billExternalReferenceNo' => $order_id,
+//     'billTo'                  => $row['first_name']." ".$row['last_name'],
+//     'billEmail'               => $row['email'],
+//     'billPhone'               => $row['phone_number'],
+//     'billSplitPayment'        => 0,
+//     'billSplitPaymentArgs'    => '',
+//     'billPaymentChannel'      => '0',
+//     'billContentEmail'        => 'Thank you for purchasing our product!',
+//     'billChargeToCustomer'    => 1
+// );
 
-// php curl to post data to payment gateway
-$curl = curl_init();
-curl_setopt($curl, CURLOPT_POST, 1);
-curl_setopt($curl, CURLOPT_URL, 'https://toyyibpay.com/index.php/api/createBill');
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($curl, CURLOPT_POSTFIELDS, $post_data);
+// // php curl to post data to payment gateway
+// $curl = curl_init();
+// curl_setopt($curl, CURLOPT_POST, 1);
+// curl_setopt($curl, CURLOPT_URL, 'https://toyyibpay.com/index.php/api/createBill');
+// curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+// curl_setopt($curl, CURLOPT_POSTFIELDS, $post_data);
 
-$result = curl_exec($curl);
-$info = curl_getinfo($curl);
-curl_close($curl);
-$result = json_decode($result, true);
+// $result = curl_exec($curl);
+// $info = curl_getinfo($curl);
+// curl_close($curl);
+// $result = json_decode($result, true);
 
-// echo '<pre>';
-// print_r($result);
-// echo '</pre>';
-// exit;
+// // echo '<pre>';
+// // print_r($result);
+// // echo '</pre>';
+// // exit;
 
-$post_data['billCode'] = $result[0]['BillCode'];
-$post_data['paymentURL'] = 'https://toyyibpay.com/' . $result[0]['BillCode'];
+// $post_data['billCode'] = $result[0]['BillCode'];
+// $post_data['paymentURL'] = 'https://toyyibpay.com/' . $result[0]['BillCode'];
 
-header('Location: ' . $post_data['paymentURL']);
+// header('Location: ' . $post_data['paymentURL']);
 
 ?>
