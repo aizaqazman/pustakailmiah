06529ceb908f9d4e9e45416aaddc360eafd211b7
Amirrul <85982648+SambalSotong@users.noreply.github.com>
2021-12-30 21:42:14 +0800

    Add files via upload

---
+++ src/index.php
@@ -0,0 +1,212 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+  <link rel="stylesheet" href="contact.css">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+  <title>Main Page</title>
+</head>
+
+<body>
+
+  <?php include 'navbar.php'; ?>
+
+<br><br>
+
+<div class="mt-4 p-5 bg-maroon text-white">
+  <h1>Pustaka Ilmiah</h1>
+  <p>This is some text</p>
+</div>
+
+<br><br>
+
+  <!-- Project Section -->
+  <div class="container-fluid text-center text-white bg-maroon p-3">
+  <h1>BEST SELLING</h1>
+  <p>This is some text.</p>
+</div>
+
+
+<br><br>
+
+
+<div class="row text-center">
+    <div class="card col m-1">
+      <img class="card-body img-fluid mx-auto" width="200px" src="product_img/koleksi_UPSR.jpg" alt="koleksi_UPSR">
+          <div>KOLEKSI UPSR 2021 TAHUN 4, 5 DAN 6</div>
+		      <div><b> RM2.95</b></div>
+    </div>
+
+    <div class="card col m-1">
+      <img class="card-body img-fluid mx-auto" width="200px" src="product_img/motivasi_umum.jpg" alt="motivasi_umum">
+	       <div>Etika Profesionalisme - SIRI KOLEJ VOKASIONAL:</div>
+	       <div><b> RM21.90</b></div>
+    </div>
+  <div class="card col m-1">
+    <img class="card-body img-fluid mx-auto" width="174.5px" src="product_img/pada_suatu_hari.jpg" alt="pada_suatu_hari">
+	  <div>Pada Suatu Hari</div>
+	  <div><b> RM25.00</b></div>
+  </div>
+  <div class="card col m-1">
+    <img class="card-body img-fluid mx-auto" width="200px" src="product_img/bestseller_agama.jpg" alt="bestseller_agama">
+	<div>KEMBALI KEPADA JAWI: PANDAI MEMBACA DAN MENULIS JAWI DALAM MASA 20 JAM</div>
+	<div><b> RM5.00</b></div>
+  </div>
+</div>
+
+<br><br>
+
+<div class="row text-center">
+
+  <div class="card col m-1">
+    <img class="card-body img-fluid mx-auto" width="200px" src="product_img/belajar_sunah_Nabi.jpg" alt="belajar_sunah_Nabi">
+        <div>24 Jam Belajar Sunah Nabi</div>
+		<div><b> RM18.00</b></div>
+  </div>
+
+  <div class="card col m-1">
+    <img class="card-body img-fluid mx-auto" width="189px" src="product_img/Komik_M.jpg" alt="Komik_M">
+        <div>Filosofi Rerama</div>
+		<div><b> RM28.00</b></div>
+  </div>
+
+  <div class="card col m-1">
+    <img class="card-body img-fluid mx-auto" width="205.5px" src="product_img/islam_kata_isteri_bukan_bibik.png" alt="islam_kata_isteri_bukan_bibik">
+        <div>Islam Kata Isteri Bukan Bibik</div>
+		<div><b> RM20.00</b></div>
+  </div>
+
+  <div class="card col m-1">
+    <img class="card-body img-fluid mx-auto" src="product_img/essay_SPM.jpg" alt="essay_SPM" style="width:60%">
+        <div>Essay Writing Made Easy SPM</div>
+		<div><b> RM15.90</b></div>
+  </div>
+
+</div>
+
+<br><br>
+
+  <!-- About Section -->
+  <div class="container-fluid text-center text-white bg-maroon p-3">
+  <h1>CATEGORIES</h1>
+  <p>This is some text.</p>
+</div>
+
+
+<br><br>
+
+  <div class="row text-center" >
+  <div class="card col m-1">
+    <img class="card-body img-fluid mx-auto" src="product_img/adab_komunikasi.jpg" alt="adab_komunikasi" style="width:60%">
+      <h4>PUSTAKA ILMIAH</h4>
+	  <p class="w3-opacity">20 PRODUCTS</p>
+  </div>
+  <div class="card col m-1">
+    <img class="card-body img-fluid mx-auto" src="product_img/ainmaisarah_1_img.jpg" alt="ainmaisarah_1_img" style="width:60%">
+      <h4>AIN MAISARAH</h4>
+	  <p class="w3-opacity">1 PRODUCT</p>
+  </div>
+  <div class="card col m-1">
+    <img class="card-body img-fluid mx-auto" src="product_img/bestseller_agama.jpg" alt="bestseller_agama" style="width:60%">
+      <h4>BESTSELLER AGAMA</h4>
+	  <p class="w3-opacity">1 PRODUCT</p>
+  </div>
+  <div class="card col m-1">
+    <img class="card-body img-fluid mx-auto" src="product_img/icon.jpg" alt="icon" style="width:60%">
+      <h4>ICON</h4>
+	  <p class="w3-opacity">1 PRODUCT</p>
+  </div>
+</div>
+
+<br><br>
+
+  <div class="row text-center">
+  <div class="card col m-1">
+    <img class="card-body img-fluid mx-auto" src="product_img/kelab_penulis_muda.jpg" alt="kelab_penulis_muda" style="width:60%">
+      <h4>KELAB PENULIS MUDA</h4>
+	  <p class="w3-opacity">1 PRODUCT</p>
+  </div>
+  <div class="card col m-1">
+    <img class="card-body img-fluid mx-auto" src="product_img/keluarga.png" alt="keluarga" style="width:60%">
+      <h4>KELUARGA</h4>
+	  <p class="w3-opacity">1 PRODUCT</p>
+  </div>
+  <div class="card col m-1">
+    <img class="card-body img-fluid mx-auto" src="product_img/Komik_M.jpg" alt="Komik_M" style="width:60%">
+      <h4>KOMIK M</h4>
+	  <p class="w3-opacity">1 PRODUCT</p>
+  </div>
+  <div class="card col m-1">
+    <img class="card-body img-fluid mx-auto" src="product_img/motivasi_umum.jpg" alt="motivasi_umum" style="width:60%">
+      <h4>MOTIVASI UMUM</h4>
+	  <p class="w3-opacity">1 PRODUCT</p>
+  </div>
+</div>
+
+<br><br>
+
+ <div class="row text-center">
+  <div class="card col m-1">
+    <img class="card-body img-fluid mx-auto" src="product_img/novel_sejarah.png" alt="novel_sejarah" style="width:60%">
+      <h4>NOVEL SEJARAH</h4>
+	  <p class="w3-opacity">1 PRODUCT</p>
+  </div>
+  <div class="col-9">&nbsp</div>
+</div>
+
+<br><br>
+
+<div class="container-fluid text-center text-white bg-maroon p-3">
+<h1>LATEST NEWS</h1>
+<p>This is some text.</p>
+</div>
+
+<br>
+
+<!-- Carousel -->
+<div id="demo" class="carousel slide p-5 m-5" data-bs-ride="carousel">
+
+  <!-- Indicators/dots -->
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#demo" data-bs-slide-to="0" class="active"></button>
+    <button type="button" data-bs-target="#demo" data-bs-slide-to="1"></button>
+    <button type="button" data-bs-target="#demo" data-bs-slide-to="2"></button>
+  </div>
+
+  <!-- The slideshow/carousel -->
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="img/image1.jpg" alt="Los Angeles" class="d-block w-100">
+    </div>
+    <div class="carousel-item">
+      <img src="img/image2.jpg" alt="Chicago" class="d-block w-100">
+    </div>
+    <div class="carousel-item">
+      <img src="img/image3.jpg" alt="New York" class="d-block w-100">
+    </div>
+  </div>
+
+  <!-- Left and right controls/icons -->
+  <button class="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </button>
+</div>
+
+
+<body>
+  <?php include 'footer.php';?>
+</body>
+
+
+</html>
+
+<!-- cantikkan pustaka ILMIAH
+      button utk semua buku
+      button utk semua kategori
